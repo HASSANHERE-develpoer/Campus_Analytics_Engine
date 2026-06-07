631360f9a0586307c92b24dcaa11ca27610d77ad
HASSANHERE-develpoer <f2025376045@umt.edu.pk>
2026-06-08 01:04:47 +0500

    Create analytics.h

---
+++ analytics.h
@@ -0,0 +1,14 @@
+#ifndef ANALYTICS_H
+#define ANALYTICS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// M3: Analytics Core Functions Declarations
+double calculateCourseAverage(const string& courseCode);
+void findCourseTopper(const string& courseCode);
+void printPerformanceReport(const string& courseCode);
+
+#endif
