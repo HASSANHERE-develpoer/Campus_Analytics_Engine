6c91469007fcc2d80c7cb1f3a01d0a290d78314e
HASSANHERE-develpoer <f2025376045@umt.edu.pk>
2026-06-14 13:08:31 +0500

    Update filehandler.h

--- filehandler.h
+++ filehandler.h
@@ -1,15 +1,9 @@
 #ifndef FILEHANDLER_H
 #define FILEHANDLER_H
 
-#include <iostream>
-#include <fstream>
 #include <string>
-#include <vector>
-
 using namespace std;
 
-vector<string> readTXT(const string& filename);
-bool appendTXT(const string& filename, const string& row);
-string getColumnValue(const string& row, int colIndex);
+string getColumnValue(const string& line, int columnIndex);
 
 #endif
