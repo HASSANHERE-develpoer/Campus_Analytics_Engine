113e19742b3e9e46ed5a0c5ec04ed063c74a2e73
HASSANHERE-develpoer <f2025376045@umt.edu.pk>
2026-06-14 13:07:20 +0500

    Update attendance.h

--- attendance.h
+++ attendance.h
@@ -1,14 +1,10 @@
 #ifndef ATTENDANCE_H
 #define ATTENDANCE_H
 
-#include <iostream>
 #include <string>
-
 using namespace std;
 
-
-void markAttendance(const string& logId, const string& enrollId, const string& date, const string& status);
-double calculateAttendancePercentage(const string& enrollId);
-void generateAttendanceReport(const string& enrollId);
+void markAttendance(const string& rollNo, const string& courseCode, const string& status);
+void viewAttendanceReport(const string& rollNo);
 
 #endif
