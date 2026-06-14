4b74cc12dc76e992d5c7cf630207c1e96872ac66
HASSANHERE-develpoer <f2025376045@umt.edu.pk>
2026-06-14 13:06:40 +0500

    Update auth.h

--- auth.h
+++ auth.h
@@ -1,12 +1,9 @@
 #ifndef AUTH_H
 #define AUTH_H
 
-#include <iostream>
 #include <string>
-
 using namespace std;
 
-// Module 1: Authentication Functions
-bool loginUser(const string& email, const string& password, string& loggedInId, string& role);
+string login(const string& username, const string& password);
 
 #endif
