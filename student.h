907e4ab615f0f3dcb72a39fe28747c2240bce662
HASSANHERE-develpoer <f2025376045@umt.edu.pk>
2026-06-14 10:59:57 +0500

    Create student.h

---
+++ student.h
@@ -0,0 +1,13 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Module 2: Student Record Management Functions
+void addStudent();
+void displayStudentProfile(const string& rollNo);
+
+#endif
