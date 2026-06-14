f3410e6dcd38b8fcaa1334fe474c819769c12a32
HASSANHERE-develpoer <f2025376045@umt.edu.pk>
2026-06-14 11:02:53 +0500

    Create course.h
    Add course.h module interface for campus listings

---
+++ course.h
@@ -0,0 +1,13 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Module 3: Course & Enrollment Management Functions
+void addCourse();
+void viewCourseCatalog();
+
+#endif
